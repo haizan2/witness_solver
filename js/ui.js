0d85a08529995f45f23d50adca06bc90fca127f8
Fabian Kloiber <fabian.kloiber@stud.uni-regensburg.de>
2016-02-11 21:52:27 +0100

    Color menu

--- js/ui.js
+++ js/ui.js
@@ -2,6 +2,8 @@ function initialize_ui()
 {
     $('[data-toggle="tooltip"]').tooltip();
     $('.menu-toggle-btn').on('focus', function(){this.blur();}).on('click', menu_toggle_btn_click);
+    $('.menu-color-select').click(menu_color_select_click);
+    $('#menu-color-input').on('input', menu_color_input_change());
 }
 
 function menu_toggle_btn_click()
@@ -12,5 +14,29 @@ function menu_toggle_btn_click()
         // ...
     } else {
         $(this).addClass('active');
+    }
+}
+
+function menu_color_select_click()
+{
+    var color = $(this).attr('fill');
+    $('#menu-color-current').attr('fill', color);
+    $('#menu-color-input').val(color.toUpperCase());
+    $('.menu-constr-shape').attr('fill', color);
+}
+
+function menu_color_input_change()
+{
+    var oldText = "#000000"
+    return function() {
+        var newText = '#' + /#*(.*)/.exec($(this).val())[1].toUpperCase();
+        if(/^[0-9A-F]{0,6}$/.exec(newText.substr(1))) {
+            oldText = newText;
+        }
+        $(this).val(oldText);
+        if(oldText.length == 4 || oldText.length == 7) {
+            $('#menu-color-current').attr('fill', oldText);
+            $('.menu-constr-shape').attr('fill', oldText);
+        }
     }
 }
